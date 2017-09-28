2aef0769d6add8d58c6f66de57d56230d94666c1
Philippe Leclerc <philippeleclerc299@gmail.com>
2017-09-27 23:09:10 -0400

    Ultra basic dice rolling

--- wallBot.js
+++ wallBot.js
@@ -11,23 +11,24 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
 
-    if(!message.content.startsWith(config.prefix) || message.author.bot) return;
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
     determineCommand(message)
 });
 
-determineCommand = function(message){
+determineCommand = function (message) {
     console.log("Determining command");
     console.log(message.author.username);
 
     let args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     let command = args.shift().toLowerCase();
 
-    switch (command){
+    switch (command) {
         case COMMAND.help:
             printHelpToChannel(message);
             break;
         case COMMAND.roll:
+            rollDiceAndPrintToChannel(message, args);
             break;
         default:
             return;
@@ -35,14 +36,65 @@ determineCommand = function(message){
 };
 
 
-printHelpToChannel = function(message) {
+printHelpToChannel = function (message) {
     message.channel.send(".\n\n" +
         "== Wall bot ==\n" +
         "  The available commands are :\n" +
         "    - !help       : shows this help interface\n" +
         "    - !roll xdy   : roll a certain number of dice and sends the results back\n" +
         "===========");
+};
 
+rollDiceAndPrintToChannel = function (message, args) {
+
+    let firstSetOfDice;
+
+    try {
+        firstSetOfDice = args.shift().toLowerCase();
+
+        console.log(firstSetOfDice);
+
+        if (firstSetOfDice == null || firstSetOfDice == undefined) {
+            printRollDiceHelp(message)
+        }
+
+        if (!firstSetOfDice.includes("d")) {
+            printRollDiceHelp(message);
+        }
+
+        // Parsing die count and faces
+
+        let [numberOfDice, facesForDie, err] = firstSetOfDice.split(/d/);
+
+        if(err) {
+            printRollDiceHelp(message);
+            return;
+        }
+
+        console.log(numberOfDice);
+        console.log(facesForDie);
+
+        let result = rollDie(facesForDie);
+
+        for (let i = 0; i < numberOfDice; i++) {
+            let result = rollDie(facesForDie);
+            message.channel.send(`You rolled ${result}`);
+        }
+
+    } catch (err) {
+        printRollDiceHelp(message)
+
+    }
+
+};
+
+rollDie = function(numberOfSide) {
+    let max = Math.floor(numberOfSide);
+    return Math.floor(Math.random() * (max)) + 1;
+};
+
+printRollDiceHelp = function(message){
+    message.channel.send("Please provide a proper dice set to be rolled");
 };
 
 client.login(config.token);
