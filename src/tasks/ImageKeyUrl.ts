6ef7d78ba73efc1a626d164209ef0e3ffabee8ab
Philippe Leclerc <philippeleclerc299@gmail.com>
2018-04-03 17:26:23 -0400

    Added an interface to dictionary like image entries, added randomizer for centrum images.

--- src/tasks/ImageKeyUrl.ts
+++ src/tasks/ImageKeyUrl.ts
@@ -1,5 +1,17 @@
 export class ImageKeyUrl {
-    public static value: any = {
-        "kmr": "https://i.imgur.com/Db2owEp.png"
-    }
+
+    public static value: ImageEntry = {
+        "kmr": "https://i.imgur.com/Db2owEp.png",
+        "centrum-fire": "https://i.imgur.com/j6q1xF4.png",
+        "centrum-wind": "https://i.imgur.com/KpLXcnH.png",
+        "centrum-water": "https://i.imgur.com/YMFm4h3.png",
+        "centrum-earth": "https://i.imgur.com/hFDSfzO.png",
+        "centrum-dark": "https://i.imgur.com/C7GhFNg.png",
+        "centrum-light": "https://i.imgur.com/ZQWgpuf.png"
+    };
+
+}
+
+interface ImageEntry {
+    [Key: string]: string;
 }
--- src/tasks/ImagePoster.ts
+++ src/tasks/ImagePoster.ts
@@ -6,7 +6,14 @@ export class ImagePoster {
 
     static postImageFromKey(message: Message, lookingForKey: string): void {
 
+        let centrumElementArray: string[] = ["fire", "wind", "dark", "water", "light", "earth"];
+
+        if (lookingForKey == "centrum") {
+            lookingForKey = "centrum-"+centrumElementArray[Math.floor(Math.random() * centrumElementArray.length)]
+        }
+
         let url = ImageKeyUrl.value[lookingForKey];
+
 
         if (url) {
             message.channel.send(url);
